654e1691b77c6346d86a3df46451ae71af136ac2
VaishnaviKulkarni111 <vaishnavikulkarni1822@gmail.com>
2024-02-15 18:56:44 +0530

    first commit

---
+++ src/components/Cart/CartButton.js
@@ -0,0 +1,18 @@
+import { useDispatch } from 'react-redux';
+import classes from './CartButton.module.css';
+import { uiActions } from '../../store/UI-Slice';
+const CartButton = (props) => {
+  const dispatch = useDispatch()
+  const toggleCart =()=>{
+    dispatch(uiActions.toggle())
+  }
+
+  return (
+    <button className={classes.button} onClick={toggleCart}>
+      <span>My Cart</span>
+      <span className={classes.badge}>1</span>
+    </button>
+  );
+};
+
+export default CartButton;
